36159203a677772b62e33b00b37970ff896c8a3c
Rafael Luque <rafa_craks7@hotmail.com>
2022-10-19 23:30:08 +0200

    new test environment

--- utils/paraview.py
+++ utils/paraview.py
@@ -2,13 +2,20 @@ from paraview.simple import *
 import sys
 import os
 
-path = sys.argv[1]
+# path = sys.argv[1]
+path= '<path to your case.foam output file downloaded from SimScale >'
+
+# directory=sys.argv[2]
+directory='<path to your output folder>'
+
+decompose = False # The optional decompose flag means that the project should be open as a decomposed case. 
+                # If you don't know if your is, check wether it has multiple "processorN" subfolders. If it does have them, it is decomposed.
 
 # create a new 'OpenFOAMReader'
 foamReader = OpenFOAMReader(FileName=path)
 
 # Properties modified on foamReader
-if sys.argc==4 and sys.argv[3]=="-d":
+if decompose:
     foamReader.CaseType = 'Decomposed Case'
 
 foamReader.UpdatePipeline()
@@ -27,9 +34,8 @@ cellCenters1.VertexCells = 1
 cellCenters1.UpdatePipeline()
 
 # save data
-directory=sys.argv[2]
-
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-SaveData(directory+"/"+sys.argv[2]+".csv", proxy=cellCenters1, WriteAllTimeSteps=1)
+# SaveData(directory+"/"+sys.argv[2]+".csv", proxy=cellCenters1, WriteAllTimeSteps=1)
+SaveData(directory+"/"+'wind_at_cell_centers'+".csv", proxy=cellCenters1, WriteAllTimeSteps=1)
